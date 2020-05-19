ab6cc65324a1c57613b0897d73bcc0917fda1e0d
chaerin918 <s2018s34@e-mirim.hs.kr>
2020-05-19 10:35:27 +0900

    버튼 추가

--- FirstFormApplication/Form1.cs
+++ FirstFormApplication/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace FirstFormApplication
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form //partial 클래스로 분할하여 작성함
     {
         public Form1()
         {
